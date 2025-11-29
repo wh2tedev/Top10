5ed37164ee6c4baf28846bb06f8bac2a6488d9c6
wh2tedev <anthonyfranklinmartinezmatos@gmail.com>
2025-11-29 19:04:32 -0400

    Update Firebase API key in configuration

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@
 // Si lo dejas así, el login funcionará en modo local.
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4013A6ernZJOBg0h3GIL8ClfSrrbGH2Y",
+  apiKey: "AIzaSyD6RiclfPQ2z5jMJylk27eTuydoHD2gDCk",
   authDomain: "topd-1be54.firebaseapp.com",
   projectId: "topd-1be54",
   storageBucket: "topd-1be54.firebasestorage.app",
@@ -10,7 +10,7 @@ const firebaseConfig = {
   appId: "1:428839382390:web:ef33025bd5285fd9d09e57"
 };
 
-if (firebaseConfig.apiKey === "AQUI") {
+if (firebaseConfig.apiKey === "AIzaSyD6RiclfPQ2z5jMJylk27eTuydoHD2gDCk") {
   window.__firebaseConfigPresent = false;
 } else {
   firebase.initializeApp(firebaseConfig);
